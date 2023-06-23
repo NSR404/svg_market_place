794f4915b7c623774896dd976cc470c93caa7882
Ibraheem <ibraheem.alaoor@hotmail.com>
2023-06-23 21:17:22 +0300

    up

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,7 +24,6 @@ class DatabaseSeeder extends Seeder
     public function  seedBranchPermissions()
     {
         $contact_permissions    =   ['view_branch' ,  'create_branch' ,    'update_branch' , 'delete_branch'];
-        Permission::query()->where('name' , 'like' , '%'.'branch'.'%')->whereSection('Branches')->delete();
         foreach($contact_permissions as $contact_permission)
         {
             $permission =   [
